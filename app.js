d0cc98948c83dece37fa8307382151b2afa13f9a
Webdeckjp <63958733+webdeckjp@users.noreply.github.com>
2020-09-13 16:56:26 +0900

    finished prototype

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ window.addEventListener("load", () => {
   let lat = "";
   let api = "";
 
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let temperatureTimezone = document.querySelector(".location-timezone");
+
   document.querySelector("button").addEventListener("click", function () {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -18,6 +24,13 @@ window.addEventListener("load", () => {
         })
         .then((data) => {
           console.log(data);
+          const temperature = Math.floor(data.main.temp / 10);
+          const summary = data.weather[0].description;
+          const timezone = data.sys.country;
+
+          temperatureDescription.innerText = summary;
+          temperatureDegree.innerText = temperature;
+          temperatureTimezone.innerText = timezone;
         });
     } else {
       document.querySelector("h1").innerText = "Couldn't get Location";
