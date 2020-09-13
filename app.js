45f0bd04e79c012c9baec8195d00c1f9c82ae633
Webdeckjp <63958733+webdeckjp@users.noreply.github.com>
2020-09-13 16:36:39 +0900

    working

--- app.js
+++ app.js
@@ -3,22 +3,24 @@ window.addEventListener("load", () => {
   let lat = "";
   let api = "";
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-      key = "fb80652993638ca76c845a8b90e17e71";
-      api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
-    });
-
-    fetch(api)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
+  document.querySelector("button").addEventListener("click", function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
+        key = "fb80652993638ca76c845a8b90e17e71";
+        api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
       });
-  } else {
-    document.querySelector("h1").innerText = "Couldn't get Location";
-  }
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        });
+    } else {
+      document.querySelector("h1").innerText = "Couldn't get Location";
+    }
+  });
 });
